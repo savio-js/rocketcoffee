3d7e68bf0b67c4bcae87f69dd08957e594880b64
Sávio de Paula <saviofju7@gmail.com>
2024-05-13 09:52:59 -0300

    Add function open and close menu

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 
+const openMenu = document.querySelector('.open')
+const closeMenu = document.querySelector('.close')
+const menu = document.querySelector('.menu')
+const main = document.querySelector('#content')
 
+
+openMenu.addEventListener('click', toggleMenu)
+
+closeMenu.addEventListener('click', toggleMenu)
+
+function toggleMenu(){
+
+  openMenu.classList.toggle('hide')
+  closeMenu.classList.toggle('hide')
+  menu.classList.toggle('hide')
+  main.classList.toggle('hide')
+
+}
